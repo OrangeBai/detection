47b9bfa12cca84f801c63aec0b36ff138848981b
OrangeBai <jzp0306@gmail.com>
2020-10-28 06:06:34 +0000

    some changes

---
+++ config.py
@@ -0,0 +1,10 @@
+import os
+
+data_root = r''
+
+coco_path = os.path.join(data_root, 'COCO')
+
+coco_train_img = os.path.join(coco_path, 'train2017')
+coco_val_img = os.path.join(coco_path, 'val2017')
+coco_train_annotation = os.path.join(coco_path, 'annotations', 'instances_train2017.json')
+coco_val_annotation = os.path.join(coco_path, 'annotations', 'instances_val2017.json')
